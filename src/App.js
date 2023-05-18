2e2a9020de3200d0cbe2635d7a969a94bd0a08b2
DGgithubDG <thenumerousrandom@gmail.com>
2023-05-18 14:27:01 +0000

    css background

--- src/App.js
+++ src/App.js
@@ -4,29 +4,13 @@ import { Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Hello
-          {/* <Link to="/Jeff">
-          <button>Go to Jeffs page</button>
-            </Link> */}
-        </a>
-        <h1 className='hello'>hello</h1>
-<div>
-  <button>Hello</button>
-  <div>Testing to see if gitpod works</div>
+    <div>
+      <div id="stars"></div>
+<div id="stars2"></div>
+<div id="stars3"></div>
+<div id="title">
+
 </div>
-      </header>
     </div>
   );
 }
