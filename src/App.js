5134b2e542e9a688d7f0cbbbef812d983ebdf327
DGgithubDG <thenumerousrandom@gmail.com>
2023-06-28 10:59:09 +0000

    homepage changes

--- src/App.js
+++ src/App.js
@@ -19,54 +19,31 @@ function App() {
       <div className="text-start"> <div class="d-flex justify-content-start"><h1>About  </h1> <h1 className="ten"> Me</h1></div></div>
       </div>
 
-<div className="row1">
+{/* <div className="row1">
 <div class="row">
   <div class="col-md-8"><ul>I currently work as a full-stack developer at King J technologies In Naxxar, Malta. I am responsible for the development and functionality of online casinos.Front-end technologies I use are JavaScript with React.js and css.The back-end consists of PHP with Laravel paired along with TSQL. (Feb2023- present)</ul>
   <ul>My favorite project I have worked on is a full-stack web app that allows people to tour east Africa for the cheapest and most convenient ways possible.This was great because i learnt not only about how to make a web app responsive but also about the natural beauty of Africa.</ul>
   <ul></ul> </div>
 
   <div class="col-md-4 text-end"><img src={profilepic}></img></div>
-  {/* <img src={avatarsvg}></img> */}
-  {/* <FcBusinessman size={100}/> */}
+
 </div>
-</div>
-
-
-	  {/* <div className="container">
-	<div className="mainhome"><h2>I'm</h2>
-	<h2 className="Denis">Denis Getov</h2>
-	</div>
-	
-	</div> */}
-
-
-
-
-
-{/* <div class="container">
-  <div class="row">
-    <div class="col">
-	<h2>I'm Denis Getov</h2>
-	<h2 className="insidethehome">Full-Stack Developer</h2>
-    </div>
-    <div class="col">
-	<p className="gif text-end"><img src={svg} class="img-thumbnail"/></p>   
-    </div>
-  </div>
-  </div> */}
-  {/* <div class="container">
-  <div class="row">
-    <div class="col">
-      1 of 2
-    </div>
-    <div class="col">
-	<p className="gif text-end"><img src={avatarsvg} class="img-thumbnail"/></p>   
-    </div>
-  </div>
-  <div class="column" >
-  <p className="profilesvg text-end"><img src={profilesvg} class="img-thumbnail"/></p>   
-  </div>
 </div> */}
+<h1 className='text-center'>Denis Getov</h1>
+<hr style={{ width: '40%', margin: '0 auto', color: 'white', borderWidth: '2px' }} />
+<h6 className='text-center'>
+I currently work as a full-stack developer at King J technologies In Naxxar, Malta. I am responsible for the development and functionality of online casinos.Front-end technologies I use are JavaScript with React.js and css.The back-end consists of PHP with Laravel paired along with TSQL. (Feb2023- present)
+</h6>
+<h6 className='text-center'>
+My favorite project I have worked on is a full-stack web app that allows people to tour east Africa for the cheapest and most convenient ways possible.This was great because i learnt not only about how to make a web app responsive but also about the natural beauty of Africa.
+
+</h6>
+
+
+
+
+
+
 
 		<div className="text-center">
 <h3 className="home-link">Find Me On</h3>
